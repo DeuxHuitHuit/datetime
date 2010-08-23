80c122454155ba55e8e6e662f3765ce155a808fd
Simone Economo <my.ekoes@gmail.com>
2010-08-23 16:00:22 +0200

    Italian language

---
+++ lang/lang.it.php
@@ -0,0 +1,201 @@
+<?php
+
+	$about = array(
+		'name' => 'Italiano',
+		'author' => array(
+			'name' => 'Simone Economo',
+			'email' => 'my.ekoes@gmail.com',
+			'website' => 'http://lineheight.net',
+		),
+		'release-date' => '2010-08-23',
+	);
+
+	
+	/*
+	 * EXTENSION: Field: Date and Time
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+	
+		'Date/Time' => 
+		'Data/Ora (Calendario)',
+
+		'Date format' => 
+		'Formato',
+
+		'Use comma to separate date and time' => 
+		'Usa la virgola per separare la data dall\'ora',
+
+		'%s Allow multiple dates' => 
+		'%s Consenti l\'inserimento di più date',
+
+		'Press <code>alt</code> to add a range' => 
+		'Premi <code>alt</code> per impostare un intervallo',
+
+		'date' => 
+		'data',
+
+		'from' => 
+		'da',
+
+		'start' => 
+		'inizio',
+
+		'end' => 
+		'fine',
+
+		'to' => 
+		'a',
+		
+		'Add new date' =>
+		'Inserisci una nuova data',
+		
+		'yesterday' => 
+		'ieri', 
+		
+		'today' => 
+		'oggi', 
+		
+		'tomorrow' => 
+		'domani', 
+		
+		'now' => 
+		'ora',
+		
+		'Sunday' => 
+		'Domenica', 
+		
+		'Monday' => 
+		'Lunedì', 
+		
+		'Tuesday' => 
+		'Martedì', 
+		
+		'Wednesday' => 
+		'Mercoledì', 
+		
+		'Thursday' => 
+		'Giovedì', 
+		
+		'Friday' => 
+		'Venerdì', 
+		
+		'Saturday' => 
+		'Sabato',
+		
+		'Sun' => 
+		'Dom', 
+		
+		'Mon' => 
+		'Lun', 
+		
+		'Tue' => 
+		'Mar', 
+		
+		'Wed' => 
+		'Mer', 
+		
+		'Thu' => 
+		'Gio', 
+		
+		'Fri' => 
+		'Ven', 
+		
+		'Sat' => 
+		'Sat',
+		
+		'Su' => 
+		'Do', 
+		
+		'Mo' => 
+		'Lu', 
+		
+		'Tu' => 
+		'Ma', 
+		
+		'We' => 
+		'Me', 
+		
+		'Th' => 
+		'Gi', 
+		
+		'Fr' => 
+		'Ve',
+		
+		'Sa' => 
+		'Sa',
+		
+		'January' => 
+		'Gennaio', 
+		
+		'February' => 
+		'Febbraio', 
+		
+		'March' => 
+		'Marzo', 
+		
+		'April' => 
+		'Aprile', 
+		
+		'May' => 
+		'Maggio', 
+		
+		'June' => 
+		'Giugno', 
+		
+		'July' => 
+		'Luglio', 
+		
+		'August' => 
+		'Agosto', 
+		
+		'September' => 
+		'Settembre', 
+		
+		'October' => 
+		'Ottobre', 
+		
+		'November' => 
+		'Novembre', 
+		
+		'December' => 
+		'Dicembre',
+		
+		'Jan' => 
+		'Gen', 
+		
+		'Feb' => 
+		'Feb', 
+		
+		'Mar' => 
+		'Mar', 
+		
+		'Apr' => 
+		'Apr', 
+		
+		'May' => 
+		'Mag', 
+		
+		'Jun' => 
+		'Giu', 
+		
+		'Jul' => 
+		'Lug', 
+		
+		'Aug' => 
+		'Ago', 
+		
+		'Sep' => 
+		'Set', 
+		
+		'Oct' => 
+		'Ott', 
+		
+		'Nov' => 
+		'Nov', 
+		
+		'Dec' => 
+		'Dic'
+
+	);
