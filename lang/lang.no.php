e0dad9d514911871cf8402dfd0e84295d8105b1f
Frode Danielsen <frode@z-it.no>
2010-11-11 15:08:31 +0100

    Added norwegian translation for the extension including datejs.

---
+++ lang/lang.no.php
@@ -0,0 +1,52 @@
+<?php
+
+	$about = array(
+		'name' => '',
+		'author' => array(
+			'name' => 'Frode Danielsen',
+			'email' => 'frode@danielsen.net',
+			'website' => 'http://frode.danielsen.net/'
+		),
+		'release-date' => '2010-11-11',
+	);
+
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date/Time' => 
+		'Dato/Tid',
+
+		'Date format' => 
+		'Datoformat',
+
+		'Use comma to separate date and time' => 
+		'Bruk komma for å skille dato og tid',
+
+		'%s Allow multiple dates' => 
+		'%s Tillat flere datoer',
+
+		'Press <code>alt</code> to add a range' => 
+		'Hold inne <code>alt</code> for å velge en datoperiode',
+
+		'date' => 
+		'dato',
+
+		'from' => 
+		'fra',
+
+		'start' => 
+		'start',
+
+		'end' => 
+		'slutt',
+
+		'to' => 
+		'til',
+
+		'Add new date' => 
+		'Legg til ny dato',
+
+	);
