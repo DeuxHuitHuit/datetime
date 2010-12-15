6ae4e42a4e85ca6e3c9fcef3014e99c87667980d
Nils Hörrmann <post@nilshoerrmann.de>
2010-12-15 18:54:09 +0100

    Temporarily remove assets

--- fields/field.datetime.php
+++ fields/field.datetime.php
@@ -169,11 +169,11 @@ Class fieldDatetime extends Field {
      */
 
     function displayPublishPanel(&$wrapper, $data=NULL, $flagWithError=NULL, $fieldnamePrefix=NULL, $fieldnamePostfix=NULL) {
-		if (Administration::instance() instanceof Symphony && !is_null(Administration::instance()->Page)) {
-			Administration::instance()->Page->addScriptToHead(URL . '/extensions/datetime/assets/jquery-ui.js', 100, true);
-			Administration::instance()->Page->addScriptToHead(URL . '/extensions/datetime/assets/datetime.js', 201, false);
-			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/datetime/assets/datetime.css', 'screen', 202, false);
-		}
+//		if (Administration::instance() instanceof Symphony && !is_null(Administration::instance()->Page)) {
+//			Administration::instance()->Page->addScriptToHead(URL . '/extensions/datetime/assets/jquery-ui.js', 100, true);
+//			Administration::instance()->Page->addScriptToHead(URL . '/extensions/datetime/assets/datetime.js', 201, false);
+//			Administration::instance()->Page->addStylesheetToHead(URL . '/extensions/datetime/assets/datetime.css', 'screen', 202, false);
+//		}
 
         // title and help
         $wrapper->setValue($this->get('label') . '<i>' . __('Press <code>alt</code> to add a range') . '</i>');
