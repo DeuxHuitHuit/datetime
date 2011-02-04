e69246ea363622c1eabb5fc6a6941c7b89250976
Vlad <vlad_micutul@yahoo.com>
2011-02-04 21:32:37 +0800

    Updated the author information

---
+++ lang/lang.ro.php
@@ -0,0 +1,200 @@
+<?php
+
+	$about = array(
+		'name' => 'Română',
+		'author' => array(
+			'name' => 'Vlad Ghiţă',
+			'email' => 'vlad_micutul@yahoo.com	',
+		),
+		'release-date' => '2011-02-04',
+	);
+
+	
+	/*
+	 * EXTENSION: Field: Date and Time
+	 * Localisation strings
+	 */
+
+	$dictionary = array(
+	
+		'Date/Time' => 
+		'Data/Ora',
+
+		'Date format' => 
+		'Formatul',
+
+		'Use comma to separate date and time' => 
+		'Folosiţi virgula pentru a separa data de oră',
+
+		'%s Allow multiple dates' => 
+		'%s Permite date multiple',
+
+		'Press <code>alt</code> to add a range' => 
+		'Apasă <code>alt</code> pentru a adăuga un interval',
+
+		'date' => 
+		'data',
+
+		'from' => 
+		'de la',
+
+		'start' => 
+		'început',
+
+		'end' => 
+		'sfârşit',
+
+		'to' => 
+		'până la',
+		
+		'Add new date' =>
+		'Adaugă o nouă dată',
+		
+		'yesterday' => 
+		'ieri', 
+		
+		'today' => 
+		'azi', 
+		
+		'tomorrow' => 
+		'mâine', 
+		
+		'now' => 
+		'acum',
+		
+		'Sunday' => 
+		'Duminică', 
+		
+		'Monday' => 
+		'Luni', 
+		
+		'Tuesday' => 
+		'Marţi', 
+		
+		'Wednesday' => 
+		'Miecuri', 
+		
+		'Thursday' => 
+		'Joi', 
+		
+		'Friday' => 
+		'Vineri', 
+		
+		'Saturday' => 
+		'Sâmbătă',
+		
+		'Sun' => 
+		'Dum', 
+		
+		'Mon' => 
+		'Lun', 
+		
+		'Tue' => 
+		'Mar', 
+		
+		'Wed' => 
+		'Mie', 
+		
+		'Thu' => 
+		'Joi', 
+		
+		'Fri' => 
+		'Vin', 
+		
+		'Sat' => 
+		'Sâm',
+		
+		'Su' => 
+		'Du', 
+		
+		'Mo' => 
+		'Lu', 
+		
+		'Tu' => 
+		'Ma', 
+		
+		'We' => 
+		'Mi', 
+		
+		'Th' => 
+		'Jo', 
+		
+		'Fr' => 
+		'Vi',
+		
+		'Sa' => 
+		'Sâ',
+		
+		'January' => 
+		'Ianuarie', 
+		
+		'February' => 
+		'Februarie', 
+		
+		'March' => 
+		'Martie', 
+		
+		'April' => 
+		'Aprilie', 
+		
+		'May' => 
+		'Mai', 
+		
+		'June' => 
+		'Iunie', 
+		
+		'July' => 
+		'Iulie', 
+		
+		'August' => 
+		'August', 
+		
+		'September' => 
+		'Septembrie', 
+		
+		'October' => 
+		'Octombrie', 
+		
+		'November' => 
+		'Noiembrie', 
+		
+		'December' => 
+		'Decembrie',
+		
+		'Jan' => 
+		'Ian', 
+		
+		'Feb' => 
+		'Feb', 
+		
+		'Mar' => 
+		'Mar', 
+		
+		'Apr' => 
+		'Apr', 
+		
+		'May' => 
+		'Mai', 
+		
+		'Jun' => 
+		'Iun', 
+		
+		'Jul' => 
+		'Iul', 
+		
+		'Aug' => 
+		'Aug', 
+		
+		'Sep' => 
+		'Sep', 
+		
+		'Oct' => 
+		'Ott', 
+		
+		'Nov' => 
+		'Noi', 
+		
+		'Dec' => 
+		'Dec'
+
+	);
