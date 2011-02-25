471d15a6659fdb12c4856899d6991372f0e89059
Nils Hörrmann <post@nilshoerrmann.de>
2011-02-25 09:11:46 +0100

    Fix table syntax

--- lib/class.calendar.php
+++ lib/class.calendar.php
@@ -71,12 +71,12 @@
 							</tr>
 						</thead>
 						<tbody>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
-							<tr><td /><td /><td /><td /><td /><td /><td /></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+							<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
 						</tbody>
 					</table>
 				</div>', 
