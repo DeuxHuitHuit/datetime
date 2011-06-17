6b94001882cfbd80e5c6ae989dbe05f69f586b50
Nils Hörrmann <post@nilshoerrmann.de>
2011-06-17 19:57:33 +0200

    Initial import of French translation

---
+++ lang/lang.fr.php
@@ -0,0 +1,102 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Solutions Nitriques',
+			'email' => 'nico (at) nitriques.com',
+			'website' => 'http://www.nitriques.com/'
+		),
+		'release-date' => '2011-06-17'
+	);
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date/Time' => 
+		'Date/Heure',
+
+		'Please make sure that the Stage submodule is initialised and available at %s.' => 
+		false,
+
+		'It\'s available at %s.' => 
+		false,
+
+		'Stage not found' => 
+		false,
+
+		'Behaviour' => 
+		false,
+
+		'Allow time editing' => 
+		false,
+
+		'This will display date and time in the interface' => 
+		false,
+
+		'Allow date ranges' => 
+		false,
+
+		'This will enable range editing' => 
+		false,
+
+		'Pre-populate this field with today\'s date' => 
+		false,
+
+		'This will automatically add the current date to new entries' => 
+		false,
+
+		'Range: <code>shift</code> + click' => 
+		false,
+
+		'No Date' => 
+		false,
+
+		'Allow creation of new items' => 
+		false,
+
+		'This will add a <code>Create New</code> button to the interface' => 
+		false,
+
+		'Allow deselection of items' => 
+		false,
+
+		'This will add a <code>Remove</code> button to the interface' => 
+		false,
+
+		'Allow selection of items from a list of existing items' => 
+		false,
+
+		'This will add a search field to the interface' => 
+		false,
+
+		'Allow dropping of items' => 
+		false,
+
+		'This will enable item dropping on textareas' => 
+		false,
+
+		'Allow sorting of items' => 
+		false,
+
+		'This will enable item dragging and reordering' => 
+		false,
+
+		'Browse' => 
+		false,
+
+		'No items found.' => 
+		false,
+
+		'no results' => 
+		false,
+
+		'1 result' => 
+		false,
+
+		'{$count} results' => 
+		false,
+
+	);
