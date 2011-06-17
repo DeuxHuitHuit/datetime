ef3f5dbfd3625050eb738d0a2eca01d41b7672b4
Nicolas Brassard <nico@nitriques.com>
2011-06-17 12:06:00 -0400

    Added French translation

---
+++ lang/lang.fr.php
@@ -0,0 +1,199 @@
+<?php
+
+	$about = array(
+		'name' => 'Français',
+		'author' => array(
+			'name' => 'Solutions Nitriques',
+			'email' => 'nico (at) nitriques.com',
+			'website' => 'http://www.nitriques.com/'
+		),
+		'release-date' => '2011-06-17',
+	);
+
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date/Time' => 
+		'Date/Heure',
+
+		'Date format' => 
+		'Format de la date',
+
+		'Use comma to separate date and time' => 
+		'Utiliser deux-points (:) pour séparer la date et l\'heure',
+
+		'%s Allow multiple dates' => 
+		'%s Permettre la sélection de plusieurs dates',
+
+		'Press <code>alt</code> to add a range' => 
+		'Appuyer sur <code>alt</code> pour ajouter une durée',
+
+		'date' => 
+		'date',
+
+		'from' => 
+		'de',
+
+		'start' => 
+		'début',
+
+		'end' => 
+		'fin',
+
+		'to' => 
+		'à',
+
+		'Add new date' => 
+		'Ajouter une nouvelle date',
+
+		'yesterday' => 
+		'hier',
+		
+		'today' => 
+		'aujourd\'hui',
+		
+		'tomorrow' => 
+		'demain',
+		
+		'now' => 
+		'maintenant',
+		
+		'Sunday' => 
+		'dimanche',
+		
+		'Monday' => 
+		'lundi',
+		
+		'Tuesday' => 
+		'mardi',
+		
+		'Wednesday' => 
+		'mercredi',
+		
+		'Thursday' => 
+		'jeudi',
+		
+		'Friday' => 
+		'vendredi',
+		
+		'Saturday' => 
+		'samedi',
+		
+		'Sun' => 
+		'dim',
+		
+		'Mon' => 
+		'lun',
+		
+		'Tue' => 
+		'mar',
+		
+		'Wed' => 
+		'mer',
+		
+		'Thu' => 
+		'jeu',
+		
+		'Fri' => 
+		'ven',
+		
+		'Sat' => 
+		'sam',
+		
+		'Su' => 
+		'di',
+		
+		'Mo' => 
+		'lu',
+		
+		'Tu' => 
+		'ma',
+		
+		'We' => 
+		'me',
+		
+		'Th' => 
+		'je',
+		
+		'Fr' => 
+		've',
+		
+		'Sa' => 
+		'sa',
+		
+		'January' => 
+		'janvier',
+		
+		'February' => 
+		'février',
+		
+		'March' => 
+		'mars',
+		
+		'April' => 
+		'avril',
+		
+		'May' => 
+		'mai',
+		
+		'June' => 
+		'juin',
+		
+		'July' => 
+		'juillet',
+		
+		'August' => 
+		'août',
+		
+		'September' => 
+		'septembre',
+		
+		'October' => 
+		'octobre',
+		
+		'November' => 
+		'novembre',
+		
+		'December' => 
+		'december',
+		
+		'Jan' => 
+		'jan',
+		
+		'Feb' => 
+		'fev',
+		
+		'Mar' => 
+		'mar',
+		
+		'Apr' => 
+		'avr',
+		
+		'May' => 
+		'mai',
+		
+		'Jun' => 
+		'jun',
+		
+		'Jul' => 
+		'jul',
+		
+		'Aug' => 
+		'aou',
+		
+		'Sep' => 
+		'sep',
+		
+		'Oct' => 
+		'oct',
+		
+		'Nov' => 
+		'nov',
+		
+		'Dec' => 
+		'dec'
+
+	);
