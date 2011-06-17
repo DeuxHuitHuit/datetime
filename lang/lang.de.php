ead0150c09ffe0f9a062a3e1bd4b88ba78d97830
Nils Hörrmann <post@nilshoerrmann.de>
2011-06-17 19:57:19 +0200

    Update German localisation

--- lang/lang.de.php
+++ lang/lang.de.php
@@ -7,7 +7,7 @@
 			'email' => 'post@nilshoerrmann.de',
 			'website' => 'http://nilshoerrmann.de'
 		),
-		'release-date' => '2011-04-25'
+		'release-date' => '2011-06-17'
 	);
 
 	/**
@@ -15,46 +15,88 @@
 	 */
 	$dictionary = array(
 
-		'Date/Time' => 
-		'Datum/Uhrzeit',
-
-		'Please make sure that the Stage submodule is initialised and available at %s.' => 
-		'Bitte überprüfen Sie, ob das Submodul Stage initialisiert wurde und unter %s zur Verfügung steht.',
-
-		'It\'s available at %s.' => 
-		'Es wird unter %s zur Verfügung gestellt. ',
-
-		'Stage not found' => 
-		'Stage konnte nicht gefunden werden',
-
-		'Behaviour' => 
-		'Verhalten',
-
-		'Allow time editing' => 
-		'Zeitbearbeitung erlauben',
-
-		'This will display date and time in the interface' => 
-		'Hierdurch werden sowohl Datum als auch Uhrzeit angezeigt',
-
-		'Pre-populate this field with today\'s date' => 
-		'Mit heutigem Datum vorausfüllen',
-
-		'This will automatically add the current date to new entries' => 
-		'Dies fügt neuen Einträgen automatisch das heutige Datum hinzu',
-
-		'Range: <code>shift</code> + click' => 
-		'Zeitraum: <code>shift</code> + Klick',
+		'1 result' => 
+		'1 Ergebnis',
 
 		'Allow creation of new items' => 
 		'Erstellen von Einträgen zulassen',
 
-		'This will add a <code>Create New</code> button to the interface' => 
-		'Dies fügt einen <code>Neu-erstellen</code>-Knopf hinzu',
+		'Allow date ranges' => 
+		'Zeitdauer zulassen',
+
+		'Allow deselection of items' => 
+		'Abwahl von Einträgen zulassen',
+
+		'Allow dropping of items' => 
+		'Einfügen von Einträge erlauben',
+
+		'Allow selection of items from a list of existing items' => 
+		'Auswahl aus einer Liste bereits bestehender Einträge zulassen',
 
 		'Allow sorting of items' => 
 		'Sortieren von Einträgen erlauben.',
 
+		'Allow time editing' => 
+		'Zeitbearbeitung erlauben',
+
+		'Behaviour' => 
+		'Verhalten',
+
+		'Browse' => 
+		'Durchsuchen',
+
+		'Date/Time' => 
+		'Datum/Uhrzeit',
+
+		'No Date' => 
+		'Kein Datum',
+
+		'No items found.' => 
+		'Keine Einträge gefunden.',
+
+		'Please make sure that the Stage submodule is initialised and available at %s.' => 
+		'Bitte überprüfen Sie, ob das Submodul Stage initialisiert wurde und unter %s zur Verfügung steht.',
+
+		'Range: <code>shift</code> + click' => 
+		'Zeitraum: <code>shift</code> + Klick',
+
+		'Stage not found' => 
+		'Stage konnte nicht gefunden werden',
+
+		'This will add a <code>Create New</code> button to the interface' => 
+		'Dies fügt einen <code>Neu-erstellen</code>-Knopf hinzu',
+
+		'This will add a <code>Remove</code> button to the interface' => 
+		'Dies fügt einen <code>Löschen</code>-Knopf hinzu',
+
+		'This will add a search field to the interface' => 
+		'Dies fügt ein Suchfeld hinzu',
+
+		'This will automatically add the current date to new entries' => 
+		'Dies fügt neuen Einträgen automatisch das heutige Datum hinzu',
+
+		'This will display date and time in the interface' => 
+		'Hierdurch werden sowohl Datum als auch Uhrzeit angezeigt',
+
 		'This will enable item dragging and reordering' => 
-		'Dies ermöglicht das Verschieben und Neuordnen von Einträgen'
+		'Dies ermöglicht das Verschieben und Neuordnen von Einträgen',
+
+		'This will enable item dropping on textareas' => 
+		'Dies ermöglicht das Ziehen von Einträgen in Textfelder',
+
+		'This will enable range editing' => 
+		'Dies ermöglicht das bearbeiten von Zeiträumen',
+
+		'no results' => 
+		'Keine Suchergebnisse',
+
+		'{$count} results' => 
+		'{$count} Ergebnisse',
+
+		'It\'s available at %s.' => 
+		'Es wird unter %s zur Verfügung gestellt.',
+
+		'Pre-populate this field with today\'s date' => 
+		'Mit heutigem Datum vorausfüllen',
 
 	);
