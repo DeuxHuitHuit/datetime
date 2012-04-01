61ce4bd8ad6a7f4a2d229018eee0a0b3742c068e
Nils Hörrmann <post@nilshoerrmann.de>
2012-04-01 11:00:09 +0200

    Create calendar inside fieldDatetime

--- fields/field.datetime.php
+++ fields/field.datetime.php
@@ -70,6 +70,165 @@
 	-------------------------------------------------------------------------*/
 	
 		/**
+		 * Create date element.
+		 *
+		 * @param string $start
+		 *  start date
+		 * @param string $end
+		 *  end date
+		 * @param mixed $class
+		 *  class names that will be added to the date element
+		 * @param int $prepopulate
+		 *  if set to 1, prepopulate element with the current date 
+		 * @param int $time
+		 *  if set to 1, display time
+		 * @return XMLElement
+		 *  returns a date element
+		 */
+		public static function createDate($element, $start=NULL, $end=NULL, $class=NULL, $prepopulate=1, $time=1) {
+			$classes = array();
+					
+			// This is hacky: remove empty end dates
+			if($end == $start) {
+				$end = NULL;
+			}
+		
+			// Range
+			if(isset($end)) {
+				$classes[] = 'range';
+			}
+			
+			// Additional classes
+			if($class) {
+				$classes[] = $class;
+			}
+			
+			// Get timer
+			if($time == 1) {
+				$cutter = '<div class="timer">' .
+					self::__createTimeline('start') . 
+					self::__createTimeline('end') . 
+				'</div>';
+			}
+			
+			// Create element
+			return new XMLElement(
+				'li', 
+				'<header>
+					<div>' . 
+						self::__createDateField($element, 'start', $start, $time, $prepopulate) . 
+						self::__createDateField($element, 'end', $end, $time) . 
+				'	</div>
+				</header>
+				<div class="calendar content">' .
+					self::__createCalendar() .			
+					$cutter .
+				'</div>', 
+				array('class' => implode($classes, ' '))
+			);
+		}
+		
+		/**
+		 * Create a date input field containing the given date
+		 *
+		 * @param string $element
+		 *  the Symphony field name
+		 * @param string $type
+		 *  either 'start' or 'end'
+		 * @param string $date
+		 *  a date
+		 * @param int $time
+		 *  display the time, if set to 1; either 1 or 0
+		 * @param int $prepopulate
+		 *  prepopulate with current date, if set to 1; either 1 or 0
+		 * @return string
+		 *  returns an input field as string
+		 */		
+		private static function __createDateField($element, $type, $date, $time, $prepopulate=0) {
+		
+			// Parse date
+			if(isset($date) || $prepopulate) {
+				$parsed = Calendar::formatDate($date, $time);
+				
+				// Generate field
+				if($parsed['status'] == 'invalid') {
+					$class = 'invalid';
+				}
+			}
+			
+			// Generate field
+			return '<input type="text" name="fields[' . $element . '][' . $type . '][]" value="' . $parsed['date'] . '" data-timestamp="' . $parsed['timestamp'] . '" class="' . $type . ' ' . $class . '" /><em class="' . $type . ' label"></em>';
+		}
+		
+		private static function __createCalendar() {
+			return '<div class="date">
+				<nav>
+					<a class="previous">&#171;</a>
+					<div class="switch">
+						<ul class="months"></ul>
+						<ul class="years"></ul>
+					</div>
+					<a class="next">&#187;</a>
+				</nav>
+				<table>
+					<thead>
+						<tr>
+							<td>' . __('Sun') . '</td>
+							<td>' . __('Mon') . '</td>
+							<td>' . __('Tue') . '</td>
+							<td>' . __('Wed') . '</td>
+							<td>' . __('Thu') . '</td>
+							<td>' . __('Fri') . '</td>
+							<td>' . __('Sat') . '</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+						<tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td></tr>
+					</tbody>
+				</table>
+			</div>';
+		}
+		
+		private static function __createTimeline($type) {
+			return '<div class="timeline ' . $type . '">
+				<span class="hour1"></span>
+				<span class="hour2"></span>
+				<span class="hour3"></span>
+				<span class="hour4"></span>
+				<span class="hour5"></span>
+				<span class="hour6"></span>
+				<span class="hour7"></span>
+				<span class="hour8"></span>
+				<span class="hour9"></span>
+				<span class="hour10"></span>
+				<span class="hour11"></span>
+				<span class="hour12"></span>
+				<span class="hour13"></span>
+				<span class="hour14"></span>
+				<span class="hour15"></span>
+				<span class="hour16"></span>
+				<span class="hour17"></span>
+				<span class="hour18"></span>
+				<span class="hour19"></span>
+				<span class="hour20"></span>
+				<span class="hour21"></span>
+				<span class="hour22"></span>
+				<span class="hour23"></span>
+				<div class="range">
+					<code>0:00</code>
+					<span class="start"></span>
+					<span class="active"></span>
+					<span class="end"></span>
+				</div>
+			</div>';
+		}
+			
+		/**
 		 * Get filtering mode from string.
 		 *
 		 * @param string $string
@@ -349,7 +508,7 @@
 
 				for($i = 0; $i < count($data['start']); $i++) {
 					$list->appendChild(
-						Calendar::createDate($this->get('element_name'), $data['start'][$i], $data['end'][$i], NULL, $this->get('prepopulate'), $this->get('time'))
+						self::createDate($this->get('element_name'), $data['start'][$i], $data['end'][$i], NULL, $this->get('prepopulate'), $this->get('time'))
 					);
 				}
 			}
@@ -357,13 +516,13 @@
 			// Current date and time
 			else {
 				$list->appendChild(
-					Calendar::createDate($this->get('element_name'), NULL, NULL, NULL, $this->get('prepopulate'), $this->get('time'))
+					self::createDate($this->get('element_name'), NULL, NULL, NULL, $this->get('prepopulate'), $this->get('time'))
 				);
 			}
 
 			// Add template
 			$list->appendChild(
-				Calendar::createDate($this->get('element_name'), NULL, NULL, 'template', $this->get('prepopulate'), $this->get('time'))
+				self::createDate($this->get('element_name'), NULL, NULL, 'template', $this->get('prepopulate'), $this->get('time'))
 			);
 
 			// Append Duplicator
