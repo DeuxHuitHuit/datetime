912607e52463ca299c848625a3aba053a3429a06
alexbirukov <lord_maloy@mail.ru>
2012-11-16 15:44:23 +0600

    Add Russian language
    Add Russian language file

---
+++ lang/lang.ru.php
@@ -0,0 +1,117 @@
+<?php
+
+	$about = array(
+		'name' => 'Русский',
+		'author' => array(
+			'name' => 'Александр Бирюков',
+			'email' => 'info@alexbirukov.ru',
+			'website' => 'http://alexbirukov.ru'
+		),
+		'release-date' => '2012-07-25'
+	);
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date and Time' => 
+		'Дата и время',
+
+		'Languages included in the Date and Time Data Source' => 
+		'Языки доступные для расширения Дата и Время',
+
+		'You can add more languages in your configuration file.' => 
+		'Вы можете добавить больше языков в файле конфигурации.',
+
+		'No language specified. Please select one or more in the system preferences.' => 
+		'Не указан язык. Пожалуйста, укажите один или несколько языков в настройках системы.',
+
+		'Date/Time' => 
+		'Дата/Время',
+
+		'%s Display time' => 
+		'%s Отображать время',
+
+		'%s Allow multiple dates' => 
+		'%s Разрешить несколько дат',
+
+		'%s Enable date ranges' => 
+		'%s Разрешить диапазон дат',
+
+		'Show help' => 
+		'Показать справку',
+
+		'Hide help' => 
+		'Скрыть справку',
+
+		'Using the input fields' => 
+		'ИСпользование поля ввода',
+
+		'click' => 
+		'клик',
+
+		'Clicking the date input will open the calendar' => 
+		'Клик на поле открывает календарь выбора даты',
+
+		'double-click' => 
+		'двойной-клик',
+
+		'Double-clicking a date input will close all calendars' => 
+		'Двойной клик на поле закроет все календари',
+
+		'Double-clicking a date input will close the calendar' => 
+		'Двойной клик на поле закроет календарь',
+
+		'tab' => 
+		'таб',
+
+		'Hitting the tab key will open the range editor' => 
+		'Нажатие на кнопку Tab приведёт к открытию редактора диапазона',
+
+		'drag' => 
+		'перетаскивание',
+
+		'Dragging dates will sort the date listing' => 
+		'Перетаскивание дат приводит к их сортировке',
+
+		'Using the calendar' => 
+		'Использовать календарь',
+
+		'Clicking a date will create a single date' => 
+		'Клик на дате создаст одну дату',
+
+		'shift+click' => 
+		'shift+клик',
+
+		'Clicking a second date while holding shift will create a date range' => 
+		'Клик на второй дате с зажатым шифтом создаст диапазон дат',
+
+		'Clicking a day will create a date' => 
+		'Клик на дате создаст дату',
+
+		'Using the timer' => 
+		'Использовать таймер',
+
+		'Clicking on a time will set start and end date to the same time' => 
+		'Кликнув на шкале времени вы установите одинаковое время для начальной и конечной даты',
+
+		'Clicking on a time while holding shift will either set the time for the start date (when the time is before the current range) or for the end date (when the time is after the current range)' => 
+		'Кликнув на шкале времени с зажатым шифтом можно задать промежуток времени.',
+
+		'Clicking on a time will set the date to that time' => 
+		'Кликнув на шкале времени вы установите дату с указанием времени',
+
+		'Add date' => 
+		'Добавить дату',
+
+		'Remove' => 
+		'Удалить',
+
+		'\'%s\' is a required field.' => 
+		'\'%s\' обязательное поле.',
+
+		'No Date' => 
+		'Дата не указана',
+
+	);
