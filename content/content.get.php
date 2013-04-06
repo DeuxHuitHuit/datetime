b2056a1ad6b81772219f64215881332c1fe9c88f
Brendan Abbott <brendan@bloodbone.ws>
2013-04-06 11:03:44 +1000

    Use AjaxPage and set the content type to be application/json for the AJAX validation of dates

--- content/content.get.php
+++ content/content.get.php
@@ -1,21 +1,26 @@
 <?php
- 
+
 	/**
 	 * @package content
 	 */
 	require_once(TOOLKIT . '/class.administrationpage.php');
 	require_once(EXTENSIONS . '/datetime/lib/class.calendar.php');
-	
-	class contentExtensionDatetimeGet extends AdministrationPage {
 
-		/**
-		 * Used to fetch subsection items via an AJAX request.
-		 */
-		public function __viewIndex() {
-			echo Calendar::formatDate(General::sanitize($_GET['date']), General::sanitize($_GET['time']), NULL, true);
+	class contentExtensionDatetimeGet extends AjaxPage {
+
+		public function handleFailedAuthorisation(){
+			$this->setHttpStatus(self::HTTP_STATUS_UNAUTHORIZED);
+			$this->_Result = json_encode(array('status' => __('You are not authorised to access this page.')));
+		}
+
+		public function view(){
+			$this->_Result = Calendar::formatDate(General::sanitize($_GET['date']), General::sanitize($_GET['time']), NULL, true);
+		}
+
+		public function generate($page = null){
+			header('Content-Type: application/json');
+			echo $this->_Result;
 			exit;
 		}
 
 	}
- 
-?>
