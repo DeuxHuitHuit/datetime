87a69c2726e38190f2e996e9cd1108ef11d03591
Wannes Debusschere <info@e-xperience.be>
2017-04-04 15:09:51 +0100

    Create lang.nl.php

---
+++ lang/lang.nl.php
@@ -0,0 +1,267 @@
+<?php
+
+	$about = array(
+		'name' => 'Nederlands',
+		'author' => array(
+			'name' => 'E-xperience - Wannes Debusschere',
+			'email' => 'info (at) e-xperience.be',
+			'website' => 'http://www.e-xperience.be/'
+		),
+		'release-date' => '2017-04-04',
+	);
+
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date/Time' => 
+		'Datum/Tijd',
+
+		'Please make sure that the Stage submodule is initialised and available at %s.' => 
+		'Zorg ervoor dat de stage submodule is geïnitialiseerd en beschikbaar is in %s',
+
+		'It\'s available at %s.' => 
+		'Het is beschikbaar in %s',
+
+		'Stage not found' => 
+		'Stage niet gevonden',
+
+		'Behaviour' => 
+		'Gedrag',
+
+		'Allow time editing' => 
+		'Bewerking van tijd toestaan',
+
+		'This will display date and time in the interface' => 
+		'Dit zal de datum en tijd weer geven in de interface',
+
+		'Allow date ranges' => 
+		'Periodes toestaan',
+
+		'This will enable range editing' => 
+		'Periodes bewerken toestaan',
+
+		'Pre-populate this field with today\'s date' => 
+		'Veld initialiseren met de datum van vandaag',
+
+		'This will automatically add the current date to new entries' => 
+		'Dit zal automatisch de huidige datum aan nieuwe items toevoegen',
+
+		'Range: <code>shift</code> + click' => 
+		'Periode: <code>shift</code> + klik',
+
+		'No Date' => 
+		'Geen datum',
+
+		'Allow creation of new items' => 
+		'Maken van nieuwe items toestaan',
+
+		'This will add a <code>Create New</code> button to the interface' => 
+		'Dit zal een <code>Maak Nieuw</code> knop toevoegen aan de interface',
+
+		'Allow deselection of items' => 
+		'Deselecteren van items toestaan',
+
+		'This will add a <code>Remove</code> button to the interface' => 
+		'Dit zal een knop <code>verwijderen</code> toevoegen aan de interface',
+
+		'Allow selection of items from a list of existing items' => 
+		'Selectie toestaan van items uit een lijst van bestaande items',
+
+		'This will add a search field to the interface' => 
+		'Dit zal een zoekveld toe voegen aan de interface',
+
+		'Allow dropping of items' => 
+		'Verslepen van items toestaan',
+
+		'This will enable item dropping on textareas' => 
+		'Toelaten een item te slepen in tekstvelden',
+
+		'Allow sorting of items' => 
+		'Sorteren van items toestaan',
+
+		'This will enable item dragging and reordering' => 
+		'Dit zal toelaten het item te slepen en opnieuw te ordenen',
+
+		'Browse' => 
+		'Doorbladeren',
+
+		'No items found.' => 
+		'Geen items gevonden',
+
+		'no results' => 
+		'geen resultaten',
+
+		'1 result' => 
+		'1 resultaat',
+
+		'{$count} results' => 
+		'{$count} resultaten',
+
+		// old entries.... they should already be in symphony
+
+		'date' => 
+		'datum',
+
+		'from' => 
+		'van',
+
+		'start' => 
+		'begin',
+
+		'end' => 
+		'einde',
+
+		'to' => 
+		'tot',
+
+		'yesterday' => 
+		'gisteren',
+		
+		'today' => 
+		'vandaag',
+		
+		'tomorrow' => 
+		'morgen',
+		
+		'now' => 
+		'nu',
+		
+		'Sunday' => 
+		'zondag',
+		
+		'Monday' => 
+		'maandag',
+		
+		'Tuesday' => 
+		'dinsdag',
+		
+		'Wednesday' => 
+		'mercredi',
+		
+		'Thursday' => 
+		'donderdag',
+		
+		'Friday' => 
+		'vrijdag',
+		
+		'Saturday' => 
+		'zaterdag',
+		
+		'Sun' => 
+		'zo',
+		
+		'Mon' => 
+		'ma',
+		
+		'Tue' => 
+		'di',
+		
+		'Wed' => 
+		'wo',
+		
+		'Thu' => 
+		'do',
+		
+		'Fri' => 
+		'vr',
+		
+		'Sat' => 
+		'za',
+		
+		'Su' => 
+		'zo',
+		
+		'Mo' => 
+		'ma',
+		
+		'Tu' => 
+		'di',
+		
+		'We' => 
+		'wo',
+		
+		'Th' => 
+		'do',
+		
+		'Fr' => 
+		'vr',
+		
+		'Sa' => 
+		'za',
+		
+		'January' => 
+		'januari',
+		
+		'February' => 
+		'februari',
+		
+		'March' => 
+		'maart',
+		
+		'April' => 
+		'april',
+		
+		'May' => 
+		'mei',
+		
+		'June' => 
+		'juni',
+		
+		'July' => 
+		'juli',
+		
+		'August' => 
+		'augustus',
+		
+		'September' => 
+		'september',
+		
+		'October' => 
+		'oktober',
+		
+		'November' => 
+		'november',
+		
+		'December' => 
+		'december',
+		
+		'Jan' => 
+		'jan',
+		
+		'Feb' => 
+		'feb',
+		
+		'Mar' => 
+		'maa',
+		
+		'Apr' => 
+		'apr',
+		
+		'May' => 
+		'mei',
+		
+		'Jun' => 
+		'jun',
+		
+		'Jul' => 
+		'jul',
+		
+		'Aug' => 
+		'aug',
+		
+		'Sep' => 
+		'sep',
+		
+		'Oct' => 
+		'okt',
+		
+		'Nov' => 
+		'nov',
+		
+		'Dec' => 
+		'dec'
+
+	);
