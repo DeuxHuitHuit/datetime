573b555766ed4b2a4e8e8e97f8d83054e0193f76
Wannes Debusschere <info@e-xperience.be>
2017-05-24 18:34:28 +0100

    Portuguese translations

---
+++ lang/lang.pt.php
@@ -0,0 +1,267 @@
+<?php
+
+	$about = array(
+		'name' => 'Portuguese',
+		'author' => array(
+			'name' => 'E-xperience - Wannes Debusschere',
+			'email' => 'info (at) e-xperience.be',
+			'website' => 'http://www.e-xperience.be/'
+		),
+		'release-date' => '2017-05-23',
+	);
+
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date/Time' => 
+		'Data / Hora',
+
+		'Please make sure that the Stage submodule is initialised and available at %s.' => 
+		'Certifique-se que o sub-módulo Canal foi inicializado e está disponível em %s.',
+
+		'It\'s available at %s.' => 
+		'Está disponível em %s',
+
+		'Stage not found' => 
+		'Canal não encontrado',
+
+		'Behaviour' => 
+		'Comportamento',
+
+		'Allow time editing' => 
+		'Permitir a edição da data/hora',
+
+		'This will display date and time in the interface' => 
+		'Isto mostra a data e hora na interface',
+
+		'Allow date ranges' => 
+		'Permitir intervalos de datas',
+
+		'This will enable range editing' => 
+		'Isto permite editar períodos',
+
+		'Pre-populate this field with today\'s date' => 
+		'Pré-preencher este campo com a data de hoje',
+
+		'This will automatically add the current date to new entries' => 
+		'Isto irá adicionar automaticamente a data atual para novas publicações',
+
+		'Range: <code>shift</code> + click' => 
+		'Período: <code>tecla shift</code> + clique',
+
+		'No Date' => 
+		'Nenhuma data',
+
+		'Allow creation of new items' => 
+		'Permitir a criação de novos itens',
+
+		'This will add a <code>Create New</code> button to the interface' => 
+		'Isto irá adicionar um botão <code>Criar Novo</code> à interface',
+
+		'Allow deselection of items' => 
+		'Permitir desmarcar itens',
+
+		'This will add a <code>Remove</code> button to the interface' => 
+		'Isto irá adicionar um botão <code>Remover</code> à interface',
+
+		'Allow selection of items from a list of existing items' => 
+		'Permitir seleção de itens de uma lista de itens existentes',
+
+		'This will add a search field to the interface' => 
+		'Isto irá adicionar um campo de pesquisa na interface',
+
+		'Allow dropping of items' => 
+		'Permitir arrastar (drag) itens',
+
+		'This will enable item dropping on textareas' => 
+		'Permitir arrastar um item para dentro de campos de texto',
+
+		'Allow sorting of items' => 
+		'Permitir ordenação de itens',
+
+		'This will enable item dragging and reordering' => 
+		'Isso permitirá arrastar itens e reordenar',
+
+		'Browse' => 
+		'Navegação',
+
+		'No items found.' => 
+		'Nenhum item encontrado',
+
+		'no results' => 
+		'nenhum resultado',
+
+		'1 result' => 
+		'1 resultado',
+
+		'{$count} results' => 
+		'{$count} resultados',
+
+		// old entries.... they should already be in symphony
+
+		'date' => 
+		'data',
+
+		'from' => 
+		'de',
+
+		'start' => 
+		'início',
+
+		'end' => 
+		'fim',
+
+		'to' => 
+		'até',
+
+		'yesterday' => 
+		'ontem',
+		
+		'today' => 
+		'hoje',
+		
+		'tomorrow' => 
+		'amanhã,
+		
+		'now' => 
+		'agora',
+		
+		'Sunday' => 
+		'Domingo',
+		
+		'Monday' => 
+		'Segunda',
+		
+		'Tuesday' => 
+		'Terça',
+		
+		'Wednesday' => 
+		'Quarta',
+		
+		'Thursday' => 
+		'Quinta',
+		
+		'Friday' => 
+		'Sexta',
+		
+		'Saturday' => 
+		'Sábado',
+		
+		'Sun' => 
+		'Dom',
+		
+		'Mon' => 
+		'Seg',
+		
+		'Tue' => 
+		'Ter',
+		
+		'Wed' => 
+		'Qua',
+		
+		'Thu' => 
+		'Qui',
+		
+		'Fri' => 
+		'Sex',
+		
+		'Sat' => 
+		'Sab',
+		
+		'Su' => 
+		'Do',
+		
+		'Mo' => 
+		'2a',
+		
+		'Tu' => 
+		'3a',
+		
+		'We' => 
+		'4a',
+		
+		'Th' => 
+		'5a',
+		
+		'Fr' => 
+		'6a',
+		
+		'Sa' => 
+		'Sa',
+		
+		'January' => 
+		'Janeiro',
+		
+		'February' => 
+		'Fevereiro',
+		
+		'March' => 
+		'Março',
+		
+		'April' => 
+		'Abril',
+		
+		'May' => 
+		'Maio',
+		
+		'June' => 
+		'Junho',
+		
+		'July' => 
+		'Julho',
+		
+		'August' => 
+		'Agosto',
+		
+		'September' => 
+		'Setembro',
+		
+		'October' => 
+		'Outubro',
+		
+		'November' => 
+		'Novembro',
+		
+		'December' => 
+		'Dezembro',
+		
+		'Jan' => 
+		'Jan',
+		
+		'Feb' => 
+		'Fev',
+		
+		'Mar' => 
+		'Mar',
+		
+		'Apr' => 
+		'Abr',
+		
+		'May' => 
+		'Mai',
+		
+		'Jun' => 
+		'Jun',
+		
+		'Jul' => 
+		'Jul',
+		
+		'Aug' => 
+		'Ago',
+		
+		'Sep' => 
+		'Set',
+		
+		'Oct' => 
+		'Out',
+		
+		'Nov' => 
+		'Nov',
+		
+		'Dec' => 
+		'Dez'
+
+	);
