8d71ff37189c0e5b9bee9438baffbded54e9fee7
peterfleck <peterfleck@gmail.com>
2017-06-27 02:44:00 +0200

    Add Spanish language (#183)
    * Picked 674145b
    * Picked 6c06dbe

---
+++ lang/lang.es.php
@@ -0,0 +1,122 @@
+<?php
+
+$about = array(
+	'name' =>'Español',
+	'author' =>array(
+		'name' =>'Peter Fleck',
+		'email' =>'peterfleck@gmail.com',
+		'website' =>''
+	),
+		'release-date' => '2017-06-21'
+	);
+
+	/**
+	 * Date and Time
+	 */
+	$dictionary = array(
+
+		'Date and Time' =>
+		'Fecha y Hora',
+
+		'Languages included in the Date and Time Data Source' =>
+		'Idiomas incluidos en la Fuente de Datos de Fecha y Hora',
+
+		'You can add more languages in your configuration file.' =>
+		'Puede agregar más idiomas en su archivo de configuración.',
+
+		'No language specified. Please select one or more in the system preferences.' =>
+		'No se especifica ningún idioma. Seleccione uno o más en las preferencias del sistema.',
+
+		'Date/Time' =>
+		'Fecha/Hora',
+
+		'%s Pre-populate with current date' =>
+		'%s Utilice la fecha actual.',
+
+		'%s Display time' =>
+		'%s Mostrar la hora',
+
+		'%s Allow multiple dates' =>
+		'%s Permitir varias fechas',
+
+		'%s Enable date ranges' =>
+		'%s Activar intervalos de fechas',
+
+		'Add date' =>
+		'Añadir Fecha',
+
+		'Remove' =>
+		'Eliminar',
+
+		'No Date' =>
+		'Sin Fecha',
+
+		// Months
+
+		'January' =>
+		'enero',
+		'February' =>
+		'febrero',
+		'March' =>
+		'marzo',
+		'April' =>
+		'abril',
+		'May' =>
+		'mayo',
+		'June' =>
+		'junio',
+		'July' =>
+		'julio',
+		'August' =>
+		'agosto',
+		'September' =>
+		'septiembre',
+		'October' =>
+		'octubre',
+		'November' =>
+		'noviembre',
+		'December' =>
+		'diciembre',
+
+		// Days
+
+		'Monday' =>
+		'lunes',
+		'Tuesday' =>
+		'martes',
+		'Wednesday' =>
+		'miércoles',
+		'Thursday' =>
+		'jueves',
+		'Friday' =>
+		'viernes',
+		'Saturday' =>
+		'sábado',
+		'Sunday' =>
+		'domingo',
+
+		'Mon' =>
+		'Lu',
+		'Tue' =>
+		'Ma',
+		'Wed' =>
+		'Mi',
+		'Thu' =>
+		'Ju',
+		'Fri' =>
+		'Vi',
+		'Sat' =>
+		'Sa',
+		'Sun' =>
+		'Do',
+
+		// Others
+
+		'yesterday' =>
+		'ayer',
+		'today' =>
+		'hoy',
+		'tomorrow' =>
+		'mañana',
+
+	);
